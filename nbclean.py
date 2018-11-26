04b0a738b2249612e187323a2ae11feaa08f4b36
Rob Hague <rob.hague@cydar.co.uk>
2018-11-26 11:41:56 +0000

    Add __NO_GIT__ and improve robustness

--- nbclean.py
+++ nbclean.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import unicode_literals
+
 # Remove outputs and execution counts from a Jupyter notebook.
 # See http://pascalbugnion.net/blog/ipython-notebooks-and-git.html
 import sys
@@ -20,6 +22,7 @@ if config is False:
 # Default behaviour is to reset execution counts but leave output intact
 clean_execution_count = config.get("execution_counts", True)
 clean_output = config.get("output", False)
+clean_metadata = config.get("metadata", True)
 
 def strip_output_from_cell(cell):
     if "outputs" in cell:
@@ -27,7 +30,8 @@ def strip_output_from_cell(cell):
             cell["outputs"] = []
         elif clean_execution_count:
             for output in cell["outputs"]:
-                output["execution_count"] = None
+                if "execution_count" in output:
+                    output["execution_count"] = None
     if clean_execution_count and "execution_count" in cell:
         cell["execution_count"] = None
     return cell
@@ -35,10 +39,24 @@ def strip_output_from_cell(cell):
 newcells = []
 for cell in json_in.get("cells", []):
     metadata = cell.get('metadata', {}).get('git', {})
-    if metadata.get('stop', False):
+    if metadata.get('skip', False):
+        continue
+    elif metadata.get('stop', False):
         break
+
+    # Look for the NO_GIT tag
+    if any("__NO_GIT__" in line for line in cell.get("source",[])):
+        continue
+
     newcells.append(strip_output_from_cell(cell))
 json_in["cells"] = newcells
+
+# Strip unwanted metadata keys
+if clean_metadata:
+    unwanted_keys = ["celltoolbar"]
+    json_in["metadata"] = {k:v for k,v in json_in["metadata"].items()
+                           if k not in unwanted_keys}
+
 
 json.dump(json_in, sys.stdout, sort_keys=True, indent=1, separators=(",",": "),
           ensure_ascii=False)
